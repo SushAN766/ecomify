d8060a48d1f3520cf3ca529ada72ac649ac5bb66
SushAN766 <sushanthbs766@gmail.com>
2025-05-19 23:39:28 +0530

    Code formatting and cleanup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,7 +9,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
+    
     <div className="flex flex-col min-h-screen">
+      
       <Navbar />
       <main className="flex-grow">
         {children}
